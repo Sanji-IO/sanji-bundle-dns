c8fd3f26a9693ae4fb429a234fde9f5aff713bc4
Jordan Chen <jordan.chen@moxa.com>
2014-12-02 10:46:58 +0800

    put __main__ code into main(), take generate config function into two function

--- dns.py
+++ dns.py
@@ -12,11 +12,12 @@ logger = logging.getLogger()
 
 
 class Dns(Sanji):
+    CONFIG_PATH = "/etc/resolv.conf"
 
     def init(self, *args, **kwargs):
         path_root = os.path.abspath(os.path.dirname(__file__))
         self.model = ModelInitiator("dns", path_root, backup_interval=1)
-        self.dns_config_path = "/etc/resolv.conf"
+        self.update_config()
 
     @Route(methods="get", resource="/network/dns")
     def get(self, message, response):
@@ -24,37 +25,54 @@ class Dns(Sanji):
 
     @Route(methods="put", resource="/network/dns")
     def put(self, message, response):
-        if hasattr(message, "data"):
-            # assign message data to self.model.db
-            self.model.db["dns"] = message.data["dns"]
-            self.model.save_db()
-            # generate config
-            update_rc = self.update_config()
-            if update_rc is False:
-                return response(code=400, data={"message":
-                                                "update config error"})
-            return response(data=self.model.db)
-        return response(code=400, data={"message": "Invaild Input"})
+        if not(hasattr(message, "data")):
+            return response(code=400, data={"message": "Invaild Input"})
+        # assign message data to self.model.db
+        if not(isinstance(message.data["dns"], list)):
+            return response(code=400, data={"message": "Invalid Data"})
+        self.model.db["dns"] = message.data["dns"]
+        self.model.save_db()
+        # generate config
+        update_rc = self.update_config()
+        if update_rc is False:
+            return response(code=400, data={"message":
+                                            "update config error"})
+        return response(data=self.model.db)
 
     def update_config(self):
         try:
             # generate config string
-            conf_str = ""
-            for server in self.model.db["dns"]:
-                conf_str = conf_str + ("nameserver %s\n" % server)
-            # save config string to /etc/resolv.conf
-            with open(self.dns_config_path, "w") as f:
-                f.write(conf_str)
+            conf_str = self.generate_config()
+            # write config string to CONFIG_PATH
+            self.write_config(conf_str)
             logger.info("dns config is updated")
             return True
         except Exception as e:
             logger.debug("update config file error:%s" % e)
             return False
 
-if __name__ == '__main__':
+    def generate_config(self):
+        conf_str = ""
+        for server in self.model.db["dns"]:
+            conf_str = conf_str + ("nameserver %s\n" % server)
+        return conf_str
+
+    def write_config(self, conf_str):
+        try:
+            with open(Dns.CONFIG_PATH, "w") as f:
+                f.write(conf_str)
+                f.close()
+        except Exception as e:
+            raise e
+
+
+def main():
     FORMAT = '%(asctime)s - %(levelname)s - %(lineno)s - %(message)s'
     logging.basicConfig(level=0, format=FORMAT)
-    logger = logging.getLogger("dns")
 
     dns = Dns(connection=Mqtt())
     dns.start()
+
+if __name__ == '__main__':
+    logger = logging.getLogger("dns")
+    main()
