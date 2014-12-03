1818453501214ef6223ddcb70d2fdb1ad26210b5
Jordan Chen <jordan.chen@moxa.com>
2014-12-03 14:24:59 +0800

    modify unittest with each testcase has arrange, act, assrty three part

--- tests/test_dns.py
+++ tests/test_dns.py
@@ -7,6 +7,7 @@ from sanji.connection.mockup import Mockup
 from sanji.message import Message
 from mock import patch
 from mock import mock_open
+from mock import Mock
 
 logger = logging.getLogger()
 
@@ -29,85 +30,129 @@ class TestDnsClass(unittest.TestCase):
         self.dns.stop()
         self.dns = None
 
-    def test_get(self):
-        # case 1: check code
-        def resp(code=200, data=None):
-            self.assertEqual(code, 200)
-        self.dns.get(message=None, response=resp, test=True)
+    def test_do_get_should_return_db(self):
+
+        # arrange
+        self.dns.model.db = {"dns": ["1.1.1.1", "2.2.2.2"]}
+
+        mock_fun = Mock(code=200, data=None)
+
+        # act
+        Dns.do_get(self.dns, message=None, response=mock_fun)
+        check_data = [({"data": self.dns.model.db},)]
+
+        # assert
+        self.assertEqual(mock_fun.call_args_list, check_data)
 
     @patch("dns.Dns.update_config")
-    def test_put1(self, update_config):
-        # case 1: message donsn't has data attribute
+    def test_do_put_without_data_should_return_code_400(self, update_config):
+
+        # arrange
         message = Message({})
+        mock_fun = Mock(code=200, data=None)
+        check_data = [({"code": 400, "data": {"message": "Invalid Input"}},)]
 
-        def resp(code=200, data=None):
-            self.assertEqual(code, 400)
-            self.assertEqual(data, {"message": "Invaild Input"})
-        self.dns.put(message=message, response=resp, test=True)
+        # act
+        Dns.do_put(self.dns, message=message, response=mock_fun)
+
+        # assert
+        self.assertEqual(mock_fun.call_args_list, check_data)
 
     @patch("dns.Dns.update_config")
-    def test_put2(self, update_config):
-        # case 2: update_rc=False
+    def test_do_put_with_invalid_data_should_return_code_400(self, update_config):
+        
+        # arrange
+        message = Message({"data": {"dns": "invalid data"}})
+        mock_fun = Mock(code=200, data=None)
+        check_data = [({"code": 400, "data": {"message": "Invalid Data"}},)]
+
+        # act
+        Dns.do_put(self.dns, message=message, response=mock_fun)
+
+        # assert
+        self.assertEqual(mock_fun.call_args_list, check_data)
+
+    @patch("dns.Dns.update_config")
+    def test_do_put_with_update_config_failed_should_return_code_400(self, update_config):
+
+        # arrange
         message = Message({"data": {"dns": ["1.1.1.1", "2.2.2.2"]}})
         update_config.return_value = False
+        mock_fun = Mock(code=200, data=None)
+        check_data = [({"code": 400, "data": {"message":
+                                              "update config error"}},)]
+        # act
+        Dns.do_put(self.dns, message=message, response=mock_fun)
 
-        def resp1(code=200, data=None):
-            self.assertEqual(code, 400)
-            self.assertEqual(data, {"message": "update config error"})
-        self.dns.put(message=message, response=resp1, test=True)
+        # assert
+        self.assertEqual(mock_fun.call_args_list, check_data)
 
     @patch("dns.Dns.update_config")
-    def test_put3(self, update_config):
-        # case 3: update_rc=True
-        message = Message({"data": {"dns": ["1.1.1.1", "2.2.2.2"]}})
+    def test_do_put_with_update_config_success_should_return_code_200(self, update_config):
+        # arrange
+        self.dns.model.db = {"dns": ["1.1.1.1", "2.2.2.2"]}
+        message = Message({"data": self.dns.model.db})
         update_config.return_value = True
+        mock_fun = Mock(code=400, data=None)
+        check_data = [({"data": self.dns.model.db},)]
 
-        def resp2(code=200, data=None):
-            self.assertEqual(code, 200)
-            self.assertEqual(data, {"dns": ["1.1.1.1", "2.2.2.2"]})
-        self.dns.put(message=message, response=resp2, test=True)
+        # act
+        Dns.do_put(self.dns, message=message, response=mock_fun)
 
-    @patch("dns.Dns.update_config")
-    def test_put4(self, update_config):
-        # case 4: Invalid data
-        message = Message({"data": {"dns": "invalid data"}})
-
-        def resp(code=200, data=None):
-            self.assertEqual(code, 400)
-            self.assertEqual(data, {"message": "Invalid Data"})
-        self.dns.put(message=message, response=resp, test=True)
+        # assert
+        self.assertEqual(mock_fun.call_args_list, check_data)
 
     @patch("dns.Dns.write_config")
     @patch("dns.Dns.generate_config")
-    def test_update_config1(self, generate_config, write_config):
-        # write_config success
+    def test_update_config_with_write_config_success_should_return_True(self, generate_config, write_config):
+
+        # arrange
         generate_config.return_value = "nameserver 1.1.1.1\n" + \
                                        "nameserver 2.2.2.2\n"
+        # act
         rc = self.dns.update_config()
+
+        # assert
         self.assertEqual(rc, True)
 
     @patch("dns.Dns.write_config")
     @patch("dns.Dns.generate_config")
-    def test_update_config2(self, generate_config, write_config):
-        # write_config failed
+    def test_update_config_with_write_config_failed_should_return_False(self, generate_config, write_config):
+
+        #arrange
         generate_config.return_value = "nameserver 1.1.1.1\n" + \
                                        "nameserver 2.2.2.2\n"
         write_config.side_effect = Exception("error exception!")
+
+        # act
         rc = self.dns.update_config()
+
+        # assert
         self.assertEqual(rc, False)
 
-    def test_generate_config(self):
+    def test_generate_config_should_return_config_string(self):
+
+        # arrange
         self.dns.model.db = {"dns": ["1.1.1.1", "2.2.2.2"]}
+
+        # act
         rc = self.dns.generate_config()
+
+        # assert
         self.assertEqual(rc, "nameserver 1.1.1.1\n" + "nameserver 2.2.2.2\n")
 
-    def test_write_config1(self):
-        # case 1: open success
+    def test_write_config_should_write_config_string_to_file(self):
+
+        # arrange
         conf_str = "nameserver 1.1.1.1\n" + "nameserver 2.2.2.2\n"
+
         # patch open
         m = mock_open()
         with patch("dns.open", m, create=True) as f:
+            # act
             self.dns.write_config(conf_str)
+
+            # assert
             f.return_value.write.assert_called_once_with(conf_str)
 
 if __name__ == "__main__":
